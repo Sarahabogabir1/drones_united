e4ff9047d48cbef6bb33fc36a29792108a08c229
Sarah Abogabir <abogabirsl@vcu.edu>
2016-05-13 19:01:27 -0400

    refactored business logic to model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,14 @@
 class Location < ActiveRecord::Base 
 	belongs_to :user
 	has_many :reviews
+	
+	def level
+		if user_level == 1 
+			"beginner"
+		elsif user_level == 2
+			"intermediate"
+		else
+			"advanced"
+		end 
+	end
 end
